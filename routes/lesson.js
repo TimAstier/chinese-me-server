9494468f5f0de06c7226ec5e7fe429012617446e
Tim <timothee.astier@essec.edu>
2017-09-14 10:15:14 +0200

    Fetch lesson data based on episodeNumber and seasonNumber

--- routes/lesson.js
+++ routes/lesson.js
@@ -4,12 +4,12 @@ import LessonSerializer from '../serializers/lesson';
 import LessonGetter from '../services/lesson-getter';
 
 function get(request, response, next) {
-  LessonGetter(request.params.id)
+  LessonGetter(request.params)
     .then(episode => LessonSerializer.serialize(episode))
     .then(episode => response.send(episode))
     .catch(next);
 }
 
 module.exports = app => {
-  app.get('/api/lesson/:id', get);
+  app.get('/api/season/:seasonNumber/lesson/:lessonNumber', get);
 };
--- services/lesson-getter.js
+++ services/lesson-getter.js
@@ -1,29 +1,37 @@
 import models from '../models';
 
-export default function LessonGetter(episodeId) {
-  return models.episode
-    .findOne({
-      where: { id: episodeId },
-      include: [{
-        model: models.example,
-        required: false,
-        attributes: [
-          'id',
-          'order',
-          'chinese',
-          'pinyin',
-          'english',
-          'literalEnglish'
+export default function LessonGetter(params) {
+  const { seasonNumber, lessonNumber } = params;
+  return models.season
+    .findOne({ where: { number: seasonNumber } })
+    .then(season => {
+      return models.episode
+      .findOne({
+        where: {
+          number: lessonNumber,
+          seasonId: season.id
+        },
+        include: [{
+          model: models.example,
+          required: false,
+          attributes: [
+            'id',
+            'order',
+            'chinese',
+            'pinyin',
+            'english',
+            'literalEnglish'
+          ]
+        }],
+        order: [
+          [ models.example, 'order', 'ASC' ]
         ]
-      }],
-      order: [
-        [ models.example, 'order', 'ASC' ]
-      ]
-    })
-    .then(episode => {
-      if (!episode) {
-        throw { status: 404, message: 'episode_not_found' };
-      }
-      return episode;
+      })
+      .then(episode => {
+        if (!episode) {
+          throw { status: 404, message: 'episode_not_found' };
+        }
+        return episode;
+      });
     });
 }
