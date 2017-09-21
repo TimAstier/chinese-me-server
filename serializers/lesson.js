afc5ad252beac2ea1a7566771ba3a14289966391
Tim <timothee.astier@essec.edu>
2017-09-21 11:16:23 +0200

    Remove lesson feature

--- serializers/lesson.js
+++
@@ -1,9 +0,0 @@
-import { Serializer as JSONAPISerializer } from 'jsonapi-serializer';
-import lessonSchema from './schemas/lesson';
-
-const LessonSerializer = new JSONAPISerializer(
-  'lessons',
-  lessonSchema
-);
-
-export default LessonSerializer;
