d9f597589afd2d9fe5413bb767327d0f51ecde30
Tim <timothee.astier@essec.edu>
2017-11-19 19:15:02 +0100

    Add technical debts section to README

--- migrations/20170703093629-create-dialogs.js
+++ migrations/20170703093629-create-dialogs.js
@@ -8,6 +8,11 @@ module.exports = {
       order: { type: Sequelize.INTEGER, allowNull: false, defaultValue: 0 },
       episodeId: {
         type: Sequelize.INTEGER,
+        // BUG: Foreign key constraints are not correctly implemented
+        // in the DB schema because the following option has a typo.
+        // 'reference' should be 'references'
+        // The constraint still exists in models thanks to Sequelize
+        // This typo exists in most of the tables.
         reference: {
           model: 'episodes',
           key: 'id'
